fd4e2d232a9407cd6dbb1dad26eeaa8a2ef856bd
ProbablyEkho <probablyekho@yahoo.com>
2026-09-14 21:12:37 +0200

    patched the walk anim on players

--- src/main/java/dev/probablyekho/pbcore/mixin/LivingEntityRendererMixin.java
+++ src/main/java/dev/probablyekho/pbcore/mixin/LivingEntityRendererMixin.java
@@ -6,6 +6,7 @@ import mod.adrenix.nostalgic.tweak.config.AnimationTweak;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class LivingEntityRendererMixin {
     @Inject(method = {"render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V"}, at = {@At(ordinal = 1, value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(FFF)V")})
     private <T extends LivingEntity> void nt_player_animation$applyClassicWalkBobbing(T entity, float entityYaw, float partialTick, PoseStack poseStack, MultiBufferSource buffer, int packedLight, CallbackInfo callback) {
-        if (AnimationTweak.OLD_CLASSIC_WALK_BOBBING.get()) {
+        if (AnimationTweak.OLD_CLASSIC_WALK_BOBBING.get() && !(entity instanceof Player)) {
             ClassicWalkHelper.applyBobbing(entity, poseStack, partialTick);
         }
     }
