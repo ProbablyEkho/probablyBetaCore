6a9a1f313c3f367fe45259dd3efd563667eda6d3
ProbablyEkho <probablyekho@yahoo.com>
2026-09-26 17:57:16 +0200

    expanded bed block hitbox

--- src/main/java/dev/probablyekho/pbcore/mixin/BedBlockMixin.java
+++ src/main/java/dev/probablyekho/pbcore/mixin/BedBlockMixin.java
@@ -2,22 +2,22 @@ package dev.probablyekho.pbcore.mixin;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.BedBlock;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.CarpetBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
-@Mixin(CarpetBlock.class)
-public class CarpetBlockMixin {
+@Mixin(BedBlock.class)
+public class BedBlockMixin {
     /**
     * @author ProbablyEkho
-    * @reason Thickens carpets a bit
+    * @reason Expands beds' shape a bit
     */
     @Overwrite
     public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-      return Block.box(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D);
+      return Block.box(0.0D, 0.0D, 0.0D, 16.0D, 9.0D, 16.0D);
    }
 }
